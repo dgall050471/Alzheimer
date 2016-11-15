2d49ad474d0de4a21bf81a8ca3176b527f57fd9c
dgall050471 <dgall@ulb.ac.be>
2016-11-15 21:29:18 +0100

    code cleanup to comply with f77 fixed format

--- alz5v.f
+++ alz5v.f
@@ -59,7 +59,6 @@
         g_k_ca=56.5d0*1.0
         g_l=0.0d0
 *        g_l=0.0971d0
-*        g_l=0.5d0
 *       sinon dv/dt toujours <0 si I_inj>0
 
 *       concentrations en microM
@@ -136,7 +135,7 @@
         yout(5)=0.1d0
 
 
-
+        open(17)
         open(18)
         do n=1,i_max
 
@@ -148,10 +147,10 @@
 *		   endif
         end do
         close(18)
-		I_inj=I_inj-1d0
+		      I_inj=I_inj-1d0
         end do
         close(17)
-      end
+        end
 
 
 *-------------------------------------------------------
@@ -213,15 +212,15 @@
         double precision FK, TK, RK, A, d,ff,k_ca
         double precision dt, t_max, t_print, t, dt_print
         double precision I_na, I_k, I_ca, I_k_ca, I_l
-	double precision  m_inf, h_inf, n_inf,
+	      double precision  m_inf, h_inf, n_inf,
      1		s_inf, kca_inf
-	double precision  tau_h,
+	      double precision  tau_h,
      1		tau_s, tau_t, tau_kca
-	double precision alpha_m, beta_m
-	double precision alpha_h, beta_h
-	double precision alpha_n, beta_n
-	double precision alpha_s, beta_s
-	double precision alpha_kca, beta_kca
+	      double precision alpha_m, beta_m
+	      double precision alpha_h, beta_h
+	      double precision alpha_n, beta_n
+	      double precision alpha_s, beta_s
+	      double precision alpha_kca, beta_kca
         integer kk
 
         common/cond/g_na, g_k, g_ca, g_k_ca, g_l
@@ -234,50 +233,49 @@
         common/steps/dt, t_max, t_print, dt_print, kk
 
 * gating Ina
-	m_inf=alpha_m(y(1))/(alpha_m(y(1))+beta_m(y(1)))
-
-	h_inf=alpha_h(y(1))/(alpha_h(y(1))+beta_h(y(1)))
-	tau_h=1.0/(alpha_h(y(1))+beta_h(y(1)))
-	if(tau_h.lt.0.045d0)then
-		tau_h=0.045d0
-	endif
+	     m_inf=alpha_m(y(1))/(alpha_m(y(1))+beta_m(y(1)))
+	      h_inf=alpha_h(y(1))/(alpha_h(y(1))+beta_h(y(1)))
+	     tau_h=1.0/(alpha_h(y(1))+beta_h(y(1)))
+	      if(tau_h.lt.0.045d0)then
+		        tau_h=0.045d0
+	      endif
 
 
 * gating Ikdr
-	n_inf=alpha_n(y(1))/(alpha_n(y(1))+beta_n(y(1)))
+	      n_inf=alpha_n(y(1))/(alpha_n(y(1))+beta_n(y(1)))
 
 
 * gating Ica
-	s_inf=alpha_s(y(1))/(alpha_s(y(1))+beta_s(y(1)))
-	tau_s=1/(alpha_s(y(1))+beta_s(y(1)))
+	      s_inf=alpha_s(y(1))/(alpha_s(y(1))+beta_s(y(1)))
+	      tau_s=1/(alpha_s(y(1))+beta_s(y(1)))
 
 * gating Ikca
-	kca_inf=alpha_kca(y(1),y(5))/(alpha_kca(y(1),y(5))
+	         kca_inf=alpha_kca(y(1),y(5))/(alpha_kca(y(1),y(5))
      1    +beta_kca(y(1),y(5)))
-        tau_kca=1/(alpha_kca(y(1),y(5))
-     1    +beta_kca(y(1),y(5)))
+
+          tau_kca=1/(alpha_kca(y(1),y(5))+beta_kca(y(1),y(5)))
 
 
 * currents
 
-	I_na=g_na*m_inf**3*y(2)*(y(1)-v_na)
-	I_k=g_k*n_inf**4*1*(y(1)-v_k)
-	I_ca=g_ca*y(3)**2*1*(y(1)-v_ca)
+	      I_na=g_na*m_inf**3*y(2)*(y(1)-v_na)
+	      I_k=g_k*n_inf**4*1*(y(1)-v_k)
+	      I_ca=g_ca*y(3)**2*1*(y(1)-v_ca)
       	I_k_ca=g_k_ca*y(4)*(y(1)-v_k)
-	I_l=g_l*(y(1)-v_l)
+	      I_l=g_l*(y(1)-v_l)
 
 * Equations
 
 *     	dv/dt
-	f(1)=-1/c_m*(I_na+I_k+I_ca+I_k_ca+I_l+I_inj)
+	     f(1)=-1/c_m*(I_na+I_k+I_ca+I_k_ca+I_l+I_inj)
 *	dh/dt
-	f(2)=(h_inf-y(2))/tau_h
+	     f(2)=(h_inf-y(2))/tau_h
 *	ds/dt
-	f(3)=(s_inf-y(3))/tau_s
+	     f(3)=(s_inf-y(3))/tau_s
 *	dkca_act/dt
-	f(4)=(kca_inf-y(4))/tau_kca
+	     f(4)=(kca_inf-y(4))/tau_kca
 *	dcai/dt
-	f(5)=ff*(-I_ca/(2*FK*A*d*1d-15)-k_ca*y(5))
+	     f(5)=ff*(-I_ca/(2*FK*A*d*1d-15)-k_ca*y(5))
 * 1 L=10^15 um^3
 
       return
@@ -286,7 +284,7 @@
 *--------------------------------------------------------
       subroutine output(t_sol, y)
 *--------------------------------------------------------
-      implicit none
+        implicit none
 
 
         double precision y(5), f(5)
@@ -297,18 +295,18 @@
         double precision FK, TK, RK, A, d,ff,k_ca
         double precision dt, t_max, t_print, t_sol, dt_print
         double precision I_na, I_k, I_ca, I_k_ca, I_l
-	    double precision  m_inf, h_inf, n_inf,
+	      double precision  m_inf, h_inf, n_inf,
      1		s_inf, kca_inf
-	    double precision  tau_h,
+	      double precision  tau_h,
      1		tau_s, tau_t, tau_kca
-	    double precision alpha_m, beta_m
-	    double precision alpha_h, beta_h
-	    double precision alpha_n, beta_n
-	    double precision alpha_s, beta_s
-	    double precision alpha_kca, beta_kca
+	      double precision alpha_m, beta_m
+	      double precision alpha_h, beta_h
+	      double precision alpha_n, beta_n
+	      double precision alpha_s, beta_s
+	      double precision alpha_kca, beta_kca
 
-	    double precision max1,max2,maxt1,maxt2
-	    integer nm, kk
+	      double precision max1,max2,maxt1,maxt2
+	      integer nm, kk
 
 
 
@@ -323,40 +321,39 @@
 	      common/maxi/nm
 	      common/maxr/max1,max2,maxt1,maxt2
 
-	I_na=g_na*m_inf**3*y(2)*(y(1)-v_na)
-	I_k=g_k*n_inf**4*1*(y(1)-v_k)
-	I_ca=g_ca*y(3)**2*1*(y(1)-v_ca)
-	I_k_ca=g_k_ca*y(4)*(y(1)-v_k)
-	I_l=g_l*(y(1)-v_l)
+	      I_na=g_na*m_inf**3*y(2)*(y(1)-v_na)
+	      I_k=g_k*n_inf**4*1*(y(1)-v_k)
+	      I_ca=g_ca*y(3)**2*1*(y(1)-v_ca)
+	      I_k_ca=g_k_ca*y(4)*(y(1)-v_k)
+	      I_l=g_l*(y(1)-v_l)
 
         if(t_sol.ge.t_print)then
         if(abs(t_sol-(kk*dt_print)).lt.0.001d0) then
            	write(18,10)t_sol-t_print, y(1), y(5)
      1		,I_na, I_k, I_ca, I_k_ca, I_l
         	call flush(18)
-			kk=kk+1
+			  kk=kk+1
 * pour detecter amplitude max
            if((max2.gt.y(1)).and.(max2.gt.max1)
      1       .and.(max2.ge.-20.d0))then
-	   if(nm.eq.0)then
+	         if(nm.eq.0)then
               		maxt1=t_sol
-			write(6,*)'maxt1',maxt1
-			write(6,*)'max1',max1
-			nm=1
-
-	   elseif(nm.eq.1)then
-		maxt2=t_sol
-		write(6,*)'maxt2',maxt2
-		write(6,*)'max2',max2
+			              write(6,*)'maxt1',maxt1
+			              write(6,*)'max1',max1
+			            nm=1
+	        elseif(nm.eq.1)then
+		          maxt2=t_sol
+		            write(6,*)'maxt2',maxt2
+		            write(6,*)'max2',max2
 
 	        if(max2.ge.-20.d0)then
      			write(17,*)I_inj,1000.d0/(maxt2-maxt1),t_sol
-			call flush(17)
-			write(6,*)I_inj,1000.d0/(maxt2-maxt1)
-			nm=2
-		endif
-	   endif
-	 endif
+			       call flush(17)
+			          write(6,*)I_inj,1000.d0/(maxt2-maxt1)
+			             nm=2
+		               endif
+	                endif
+	               endif
            endif
            max1=max2
            max2=y(1)
@@ -373,95 +370,92 @@
 
 * gating Ina
 *--------------------------------------------------------
-	function alpha_m(v)
+	     function alpha_m(v)
 *--------------------------------------------------------
-	implicit none
+	      implicit none
         double precision v, alpha_m
-	alpha_m=7.5d0*dexp(0.081d0*(v+39.d0))
-	return
-	end
+	       alpha_m=7.5d0*dexp(0.081d0*(v+39.d0))
+	        return
+	       end
 *--------------------------------------------------------
-	function beta_m(v)
+	     function beta_m(v)
 *--------------------------------------------------------
-	implicit none
+	      implicit none
         double precision v, beta_m
-	beta_m=7.5d0*dexp(-0.066d0*(v+39.d0))
-	return
-	end
+	       beta_m=7.5d0*dexp(-0.066d0*(v+39.d0))
+	        return
+	       end
 *--------------------------------------------------------
-	function alpha_h(v)
+	     function alpha_h(v)
 *--------------------------------------------------------
-	implicit none
+	     implicit none
         double precision v, alpha_h
-	alpha_h=0.6d0*dexp(-0.089d0*(v+50.d0))
-	return
-	end
+	       alpha_h=0.6d0*dexp(-0.089d0*(v+50.d0))
+	        return
+	       end
 *--------------------------------------------------------
-	function beta_h(v)
+	       function beta_h(v)
 *--------------------------------------------------------
-	implicit none
+	      implicit none
         double precision v, beta_h
-	beta_h=0.6d0*dexp(0.089d0*(v+50.d0))
-	return
-	end
+	       beta_h=0.6d0*dexp(0.089d0*(v+50.d0))
+	        return
+	       end
 
 
 
 * gating Ikdr
 *--------------------------------------------------------
-	function alpha_n(v)
+	     function alpha_n(v)
 *--------------------------------------------------------
-	implicit none
+	      implicit none
         double precision v, alpha_n
-	alpha_n=0.85d0*dexp(0.073d0*(v+38.d0))
-	return
-	end
+	        alpha_n=0.85d0*dexp(0.073d0*(v+38.d0))
+	         return
+	        end
 *--------------------------------------------------------
-	function beta_n(v)
+	     function beta_n(v)
 *--------------------------------------------------------
-	implicit none
+	     implicit none
         double precision v, beta_n
-	beta_n=0.85d0*dexp(-0.018d0*(v+38.d0))
-	return
-	end
-
-
+	       beta_n=0.85d0*dexp(-0.018d0*(v+38.d0))
+	        return
+	       end
 
 * gating Ica
 *--------------------------------------------------------
-	function alpha_s(v)
+	     function alpha_s(v)
 *--------------------------------------------------------
-	implicit none
+	      implicit none
         double precision v, alpha_s
-	alpha_s=8.0d0/(1+dexp(-0.072d0*(v-5.d0)))
-	return
-	end
+	       alpha_s=8.0d0/(1+dexp(-0.072d0*(v-5.d0)))
+	        return
+	       end
 *--------------------------------------------------------
-	function beta_s(v)
+	     function beta_s(v)
 *--------------------------------------------------------
-	implicit none
+	      implicit none
         double precision v, beta_s
-	beta_s=0.1d0*(v+8.9)/(dexp(0.2d0*(v+8.9d0))-1)
-	return
-	end
+	       beta_s=0.1d0*(v+8.9)/(dexp(0.2d0*(v+8.9d0))-1)
+	        return
+	       end
 
 
 * gating Ikca
 *--------------------------------------------------------
-	function alpha_kca(v,ca)
+	     function alpha_kca(v,ca)
 *--------------------------------------------------------
-	implicit none
+	      implicit none
         double precision v,ca, alpha_kca
-	alpha_kca=12.5d0/(1+(1.5*0.1*dexp(-0.085d0*v)/ca))
-	return
-	end
-
+	        alpha_kca=12.5d0/(1+(1.5*0.1*dexp(-0.085d0*v)/ca))
+	         return
+	        end
 
 *--------------------------------------------------------
-	function beta_kca(v,ca)
+	     function beta_kca(v,ca)
 *--------------------------------------------------------
-	implicit none
+        implicit none
         double precision v, ca,  beta_kca
-	beta_kca=7.5d0/(1+(ca/(0.15*0.1*dexp(-0.077d0*v))))
-	return
-	end
+	       beta_kca=7.5d0/(1+(ca/(0.15*0.1*dexp(-0.077d0*v))))
+	        return
+	       end
